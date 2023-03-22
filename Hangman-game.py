f0ac93df393c131f971648a9a258d59b4024473c
Lorena Melo <120689570+lorenamelos@users.noreply.github.com>
2023-03-22 16:57:09 -0300

    Update Hangman-game.py

--- Hangman-game.py
+++ Hangman-game.py
@@ -1,67 +1,14 @@
 '''This project was part of the course '100 Days of Code: The Complete Python Pro Bootcamp for 2023'. '''
 
+#importing the libraries etc
 import random
-import string
+from hangman_words import word_list
+from hangman_art import logo, stages
+import os
 
-word_list = ["aardvark", "baboon", "camel"]
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
 
 ''' - Randomly choose a word from the word_list and assign it to a variable called chosen_word.'''
+
 chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
 
@@ -78,28 +25,36 @@ for _ in range(word_length):
 end_of_game = False
 lives = 6
 
+print(logo)
 
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
 
+    os.system("clear")
+    
+    if guess in display:
+       print(f"Youǘe already guesses {guess}")       
+
     for position in range(word_length):
-      letter = chosen_word[position]
-      #print(f'Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}' )
+      letter = chosen_word[position]     
       if letter == guess:
         display[position] = letter
        
     if guess not in chosen_word:
+      print(f"You've guessed {guess}, that's not in the word. You lose a life.")
       lives -= 1
       if lives == 0:
         end_of_game = True
         print("You lose.")
+        print(f"The world is: {chosen_word}")
     
     print(f"{' '.join(display)}")
 
     if "_" not in display:
       end_of_game = True
       print('You win.')
-        
+
+    from hangman_art import stages    
     print(stages[lives])
 
 
